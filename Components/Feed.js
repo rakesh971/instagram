74d742d611905b40d963e9985bf034bd3db6f6db
amolmugnusmare <amol20@navgurukul.org>
2022-05-14 16:45:03 +0530

    populate fake data using minifaker and create feed and story component

---
+++ Components/Feed.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Stories from './Stories'
+
+const Feed = () => {
+  return (
+    <div>
+        <section>
+        <Stories/>
+        </section>
+        <section>
+
+        </section>
+    </div>
+  )
+}
+
+export default Feed
--- Components/Header.js
+++ Components/Header.js
@@ -5,35 +5,37 @@ import { HomeIcon } from "@heroicons/react/solid"
 
 const Header = () => {
   return (
-    <div className='flex items-center justify-between max-w-6xl mx-4 xl:mx-auto'>
-        <div className='cursor-pointer h-24 w-24 relative hidden lg:inline-grid'>
-            <Image
-                src="https://cdn2.downdetector.com/static/uploads/logo/Instagram_Logo_Large.png"
-                layout='fill'
-                className='object-contain'
-            />
-        </div>
-        <div className='cursor-pointer h-24 w-10 relative  lg:hidden'>
-            <Image
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/1200px-Instagram_logo_2016.svg.png"
-                layout='fill'
-                className='object-contain'
-            />
-        </div>
-        <div className='relative mt-1'>
-            <div className='absolute top-2 left-2'>
-                <SearchIcon className='h-5 text-gray-500'/>
+   <div className='shadow-sm border-b sticky top-0 bg-white z-30'>
+        <div className='flex items-center justify-between max-w-6xl mx-4 xl:mx-auto'>
+            <div className='cursor-pointer h-24 w-24 relative hidden lg:inline-grid'>
+                <Image
+                    src="https://cdn2.downdetector.com/static/uploads/logo/Instagram_Logo_Large.png"
+                    layout='fill'
+                    className='object-contain'
+                />
             </div>
-            <input type="text" placeholder='Search' className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md'/>
-        </div>
-       <div className='flex space-x-4 items-center'>
-             <HomeIcon className= "hidden md:inline-flex  h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out"/>
-            <PlusCircleIcon
-                className="h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out"
-            />
-            <img src="https://new-img.patrika.com/upload/2022/03/27/ram-charan-next-target-mother-dream.jpg" alt ="user-image" className='h-10 w-10 rounded-full cursor-pointer object-cover'/>
-       </div>
-    </div>
+            <div className='cursor-pointer h-24 w-10 relative  lg:hidden'>
+                <Image
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/1200px-Instagram_logo_2016.svg.png"
+                    layout='fill'
+                    className='object-contain'
+                />
+            </div>
+            <div className='relative mt-1'>
+                <div className='absolute top-2 left-2'>
+                    <SearchIcon className='h-5 text-gray-500'/>
+                </div>
+                <input type="text" placeholder='Search' className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md'/>
+            </div>
+            <div className='flex space-x-4 items-center'>
+                    <HomeIcon className= "hidden md:inline-flex  h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out"/>
+                    <PlusCircleIcon
+                        className="h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out"
+                    />
+                    <img src="https://new-img.patrika.com/upload/2022/03/27/ram-charan-next-target-mother-dream.jpg" alt ="user-image" className='h-10 w-10 rounded-full cursor-pointer object-cover'/>
+            </div>
+         </div>
+   </div>
   )
 }
 
