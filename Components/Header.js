77e8af28fe025e0f83c6ee9d9b88875c7ff2390d
amolmugnusmare <amol20@navgurukul.org>
2022-05-14 15:37:03 +0530

    add the menu annd profile image at the right side of the header

--- Components/Header.js
+++ Components/Header.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import React from 'react'
-import { SearchIcon } from "@heroicons/react/outline"
+import { SearchIcon,PlusCircleIcon } from "@heroicons/react/outline"
+import { HomeIcon } from "@heroicons/react/solid"
 
 const Header = () => {
   return (
-    <div className='flex items-center justify-between max-w-6xl'>
+    <div className='flex items-center justify-between max-w-6xl mx-4 xl:mx-auto'>
         <div className='cursor-pointer h-24 w-24 relative hidden lg:inline-grid'>
             <Image
                 src="https://cdn2.downdetector.com/static/uploads/logo/Instagram_Logo_Large.png"
@@ -25,7 +26,13 @@ const Header = () => {
             </div>
             <input type="text" placeholder='Search' className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md'/>
         </div>
-        <h1>right side</h1>
+       <div className='flex space-x-4 items-center'>
+             <HomeIcon className= "hidden md:inline-flex  h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out"/>
+            <PlusCircleIcon
+                className="h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out"
+            />
+            <img src="https://new-img.patrika.com/upload/2022/03/27/ram-charan-next-target-mother-dream.jpg" alt ="user-image" className='h-10 w-10 rounded-full cursor-pointer object-cover'/>
+       </div>
     </div>
   )
 }
