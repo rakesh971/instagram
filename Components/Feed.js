2e7ed7a46b17cb32995ae65a796573dc730533a2
amolmugnusmare <amol20@navgurukul.org>
2022-05-16 11:24:53 +0530

    create the mini profilee component

--- Components/Feed.js
+++ Components/Feed.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import MiniProfile from './MiniProfile'
 import Posts from './Posts'
 import Stories from './Stories'
 
@@ -10,7 +11,10 @@ const Feed = () => {
             <Posts/>
         </section>
         <section className='hidden md:inline-grid md:col-span-1'>
-
+          {/* mini profile */}
+          <div className='fixed w-[380px]'>
+              <MiniProfile/>
+          </div>
         </section>
     </div>
   )
