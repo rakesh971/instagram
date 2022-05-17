2db43a98256531da8af0b72fb9ca75eaf2752f19
amolmugnusmare <amol20@navgurukul.org>
2022-05-17 16:01:40 +0530

    Get the post data from database and show it in the feed section

--- Components/Posts.js
+++ Components/Posts.js
@@ -1,34 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react';
 import Post from './Post'
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { db } from '../firebase';
+import { Snapshot } from 'recoil';
 
 const Posts = () => {
-    const posts =[
-        {
-            id:"1",
-            username:"rakesh",
-            userImg:"https://new-img.patrika.com/upload/2022/03/27/ram-charan-next-target-mother-dream.jpg",
-            img:"https://images.unsplash.com/photo-1652502060260-15b075518034?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-            caption:"nice picture",
-        },
-        
-        {
-            id:"2",
-            username:"nava shreyas",
-            userImg:"https://new-img.patrika.com/upload/2022/03/27/ram-charan-next-target-mother-dream.jpg",
-            img:"https://images.unsplash.com/photo-1652510192446-b553fc3c88ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
-            caption:"mama",
-        }
-    ]
+    const [Posts,SetPosts] = useState([]);
+    useEffect(()=>{
+        const unsubscribe =onSnapshot(
+           query (collection(db,"posts"),orderBy(("timestamp"),"desc")),(Snapshot)=>{
+                SetPosts(Snapshot.docs)
+            }
+        );
+        return unsubscribe;
+    })
+  
   return (
     <div>
-        {posts.map(post=>(
+        {Posts.map(post=>(
             <Post  
                 key={post.id}
                 id={post.id}
-                username={post.username}
-                userImg={post.userImg}
-                img={post.img}
-                caption={post.caption}
+                username={post.data().username}
+                userImg={post.data().profileImg}
+                img={post.data().image}
+                caption={post.data().caption}
             />
         ))}
     </div>
