83e420f7e3bf40d29fe3e1c6b59fcf847701921f
amolmugnusmare <amol20@navgurukul.org>
2022-05-17 17:56:29 +0530

    Show comments in the comments section and get data from the firebase database

--- Components/Post.js
+++ Components/Post.js
@@ -4,11 +4,29 @@ import { useSession } from 'next-auth/react'
 import { useState } from 'react'
 import { async } from '@firebase/util'
 import { comment } from 'postcss'
-import { addDoc, collection,serverTimestamp } from 'firebase/firestore'
+import { addDoc, collection,onSnapshot,serverTimestamp ,query,orderBy} from 'firebase/firestore'
 import { db } from '../firebase'
+import { useEffect } from 'react'
+import { Snapshot } from 'recoil'
+import Moment from 'react-moment';
+
+
 const Post = ({img,username,caption,userImg,id}) => {
   const {data:session}= useSession()
-  const [Comment,SetComment] =useState("")
+  const [Comment,SetComment] =useState("");
+  const [Comments,SetComments] =useState("")
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(
+        collection(db, "posts", id, "comments"),
+        orderBy("timestamp", "desc")
+      ),
+      (snapshot) => {
+        SetComments(snapshot.docs);
+      }
+    );
+  }, [db, id]);
 
  async function sendComment(event){
     event.preventDefault();
@@ -44,7 +62,26 @@ const Post = ({img,username,caption,userImg,id}) => {
        }
 
        {/* post comments */}
-       <p className='p-5 truncate'><span className='foonnt-bold mr-2'>{username}</span><span>{caption}</span></p>
+       <p className='p-5 truncate'><span className='font-bold mr-2'>{username}</span><span>{caption}</span></p>
+       {Comments.length > 0 && (
+        <div className="mx-10 max-h-24 overflow-y-scroll scrollbar-none">
+          {Comments.map((comment) => (
+            <div
+              key={comment.data().id}
+              className="flex items-center space-x-2 mb-2"
+            >
+              <img
+                className="h-7  rounded-full object-cover"
+                src={comment.data().userImage}
+                alt="user-image"
+              />
+              <p className="font-semibold">{comment.data().username}</p>
+              <p className="flex-1 truncate">{comment.data().comment}</p>
+              <Moment fromNow>{comment.data().timestamp?.toDate()}</Moment>
+            </div>
+          ))}
+        </div>
+      )}
         {/* post input box */}
 
         {session && (
